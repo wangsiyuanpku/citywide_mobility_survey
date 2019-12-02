39b21f815f3913abf6e44b39f0563fc12cd577da
Tim <wangtian0312@gmail.com>
2019-12-02 18:35:28 -0500

    feat: create bar chart

--- dashboard/main.js
+++ dashboard/main.js
@@ -13,9 +13,23 @@ let genderMapping = (item) =>{
     return syntax[item];
 }
 
+let ageMapping = function(item){
+    if (item <=24) {
+        return '19-24'
+    } else if (item <= 54) {
+        return '20-54'
+    } else if (item <= 64) {
+        return '55-64'
+    } else if (item <= 100){
+        return '>65'
+    } else {
+        return 'Refused to answer'
+    }
+}
+
 let identicalMapping = item => item;
 
-function pieChart(cf_data, dimensionColumn, pieChartID, mapping=identicalMapping){
+function groupData(cf_data, dimensionColumn, mapping=identicalMapping) {
     let dimension = cf_data.dimension(item => mapping(item[dimensionColumn]));
     let quantity = dimension.group().reduceCount(item => item);
     let result = quantity.all();
@@ -24,8 +38,19 @@ function pieChart(cf_data, dimensionColumn, pieChartID, mapping=identicalMapping
         console.log(dimensionColumn);
         console.log(result);
     }
+    return [dimension, quantity]
+}
 
-    var pie = dc.pieChart(pieChartID);
+function createGraphDiv(graphID, divID){
+    d3.select(`#${divID}`).append('div').attr("style", 'margin: 15px').attr("id", graphID);
+}
+
+function pieChart(cf_data, dimensionColumn, pieChartID, mapping=identicalMapping, divID='filters'){
+    createGraphDiv(pieChartID, divID)
+    let [dimension, quantity] = groupData(cf_data, dimensionColumn, mapping)
+    
+
+    var pie = dc.pieChart(`#${pieChartID}`);
     pie
         .width(200)
         .height(200)
@@ -38,7 +63,28 @@ function pieChart(cf_data, dimensionColumn, pieChartID, mapping=identicalMapping
     pie.render();
 }
 
+function barChart(cf_data, dimensionColumn, barChartID, mapping=identicalMapping, divID='filters', ...barParameters){
+    createGraphDiv(barChartID, divID)
+
+    let [dimension, quantity] = groupData(cf_data, dimensionColumn, mapping)
+
+
+    var chart = dc.barChart(`#${barChartID}`);
+    chart
+        .width(500)
+        .height(200)
+        .gap(0)
+        .x(d3.scaleOrdinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel(dimensionColumn)
+        .yAxisLabel("Count")
+        .elasticY(true)
+        .dimension(dimension)
+        .group(quantity);
+    chart.render();
+}
+
 d3.json(data_loc).then(crossfilter).then((cf_data)=>{
-    pieChart(cf_data, 'qgender', "#pie1", genderMapping);
-    pieChart(cf_data, 'qage', '#pie2', )
+    pieChart(cf_data, 'qgender', "pie1", genderMapping);
+    barChart(cf_data, 'qage', 'pie2', ageMapping)
 });
